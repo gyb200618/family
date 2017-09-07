3168cf3c4c92d95837f40ec22a7db73d310232b7
abel_guo <Abel_Guo@asus.com>
2017-09-07 22:05:51 +0800

    budget detail

--- pages/fitment/budget/detail/detail.js
+++ pages/fitment/budget/detail/detail.js
@@ -12,7 +12,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    this.initData()
   },
 
   /**
@@ -57,10 +57,62 @@ Page({
   
   },
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  initData: function() {
+    var detail = [
+      {
+        name: '边桌',
+        budget: 400,
+        model: 'JT2017',
+        price: 300,
+        count: 2,
+        seller: 'tmall',
+        tel: '13012345678',
+        showDetail: false
+      },
+      {
+        name: '茶几',
+        budget: 800,
+        model: '',
+        price: 600,
+        count: 1,
+        seller: 'tmall',
+        tel: '13012345678',
+        showDetail: false
+      },
+      {
+        name: '电视柜',
+        budget: 1000,
+        model: '白橡木',
+        price: 1200,
+        count: 1,
+        seller: '蠡口',
+        tel: '',
+        showDetail: false
+      },
+      {
+        name: '鞋柜',
+        budget: 400,
+        model: '超薄款',
+        price: 300,
+        count: 1,
+        seller: '宜家',
+        tel: '',
+        showDetail: false
+      }
+    ]
+
+    this.setData({
+      detail: detail
+    })
+  },
+
+  toggleDetail: function(e) {
+    var idx = e.currentTarget.dataset.idx
+    var show = this.data.detail[idx].showDetail
+    this.data.detail[idx].showDetail = show ? false : true
+
+    this.setData({
+      detail: this.data.detail
+    })
   }
 })
